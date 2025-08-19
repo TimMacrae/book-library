d00e285b4c4dcacea9d87513c6164e5609f13972
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-19 17:04:51 +0200

    create BooksService

---
+++ backend/src/main/java/com/ecosystem/backend/books/BooksService.java
@@ -0,0 +1,38 @@
+package com.ecosystem.backend.books;
+
+import com.ecosystem.backend.books.dto.BookDto;
+import com.ecosystem.backend.books.models.Book;
+import com.ecosystem.backend.books.repository.BooksRepo;
+import com.ecosystem.backend.exception.BookCouldNotBeCreated;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.ecosystem.backend.utility.IdService.generateId;
+
+@Service
+@AllArgsConstructor
+public class BooksService {
+
+    BooksRepo booksRepo;
+
+    public Book addBook(BookDto bookDto) {
+        try {
+            Book book = new Book(
+                    generateId(),
+                    bookDto.title(),
+                    bookDto.description(),
+                    bookDto.authors(),
+                    bookDto.firstPublishDate(),
+                    bookDto.cover(),
+                    bookDto.language(),
+                    bookDto.isbn()
+            );
+
+            return booksRepo.save(book);
+
+        } catch (Exception exception) {
+            throw new BookCouldNotBeCreated();
+        }
+
+    }
+}
