0a57dcef46bee6529390a5fde34e223cbe277ed7
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-08-21 18:52:21 +0200

    Swapped path for variable from routerConfig

--- frontend/src/components/books/BooksButtonEdit.tsx
+++ frontend/src/components/books/BooksButtonEdit.tsx
@@ -2,12 +2,13 @@ import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import Stack from '@mui/material/Stack';
 import {useNavigate} from "react-router-dom";
+import {routerConfig} from "../../pages/routerConfig.ts";
 
 
 export function BooksButtonEdit(props){
     const navigate = useNavigate();
     function goToBook(){
-        navigate(`/book/${props.id}/edit`)
+        navigate(routerConfig.URL.BOOKEDIT)
     }
     return (
         <>
