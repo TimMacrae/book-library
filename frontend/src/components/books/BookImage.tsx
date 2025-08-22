8b57a40f76d3acd1485aff609ee5746b0e463315
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-22 17:17:05 +0200

    create new BookImage component

---
+++ frontend/src/components/books/BookImage.tsx
@@ -0,0 +1,43 @@
+import type {BookWithId} from "../../types/bookType.ts";
+import {Box} from "@mui/material";
+import BrokenImageIcon from "@mui/icons-material/BrokenImage";
+
+type BookImageProps = {
+    book: BookWithId
+}
+
+export function BookImage({book}: BookImageProps) {
+    if (book.cover) {
+        return (<Box
+            component="img"
+            src={book.cover}
+            alt={book.title}
+            sx={{
+                width: "100%",
+                height: 200,
+                objectFit: "cover",
+                minWidth: 400,
+                borderTopLeftRadius: 1,
+                borderTopRightRadius: 1,
+            }}
+        />)
+    }
+
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: 200,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "grey.200",
+                borderTopLeftRadius: 1,
+                borderTopRightRadius: 1,
+            }}
+        >
+            <BrokenImageIcon sx={{fontSize: 80, color: "grey.500"}}/>
+        </Box>
+
+    )
+}
