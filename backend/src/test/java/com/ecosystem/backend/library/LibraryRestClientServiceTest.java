3507e555cf7cbf35c2f6f1a134d111d201458ed1
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-22 10:16:22 +0200

    remove  unused imports

--- backend/src/test/java/com/ecosystem/backend/library/LibraryRestClientServiceTest.java
+++ backend/src/test/java/com/ecosystem/backend/library/LibraryRestClientServiceTest.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestClient;
 
