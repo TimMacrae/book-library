551303554d90ebe1072bf15d8ebd5e99db86998a
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-22 16:59:10 +0200

    update Edit button

--- frontend/src/components/books/BooksButtonEdit.tsx
+++ frontend/src/components/books/BooksButtonEdit.tsx
@@ -3,19 +3,23 @@ import EditIcon from '@mui/icons-material/Edit';
 import Stack from '@mui/material/Stack';
 import {useNavigate} from "react-router-dom";
 import {routerConfig} from "../../pages/routerConfig.ts";
+import type {BookWithId} from "../../types/bookType.ts";
 
 
-export function BooksButtonEdit(props){
+type BooksButtonEditProps = {
+    book: BookWithId;
+}
+
+export function BooksButtonEdit({book}: BooksButtonEditProps) {
     const navigate = useNavigate();
     function goToBook(){
-        navigate(routerConfig.URL.BOOKEDIT)
+        console.log("EDIT BT:",book.id);
+        navigate(routerConfig.URL.BOOK_EDIT_ID(book.id));
     }
     return (
         <>
             <Stack direction="row" spacing={2}>
-                <Button variant="contained" endIcon={<EditIcon />} onClick={goToBook}>
-                    Edit book
-                </Button>
+                <Button variant="outlined" onClick={goToBook} > <EditIcon /></Button>
             </Stack>
         </>
     );
