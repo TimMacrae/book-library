547932e4f90c5b3acce32de2b5b15eb31759ccb0
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-22 16:58:38 +0200

    Change the grid

--- frontend/src/components/books/BooksGallery.tsx
+++ frontend/src/components/books/BooksGallery.tsx
@@ -1,7 +1,8 @@
 import {BookCard} from "./BookCard.tsx";
-import {Box, Grid} from "@mui/material";
+import Box from '@mui/material/Box';
 import {LoadingSpinner} from "../LoadingSpinner.tsx";
 import type {BookWithId} from "../../types/bookType.ts";
+import Grid from '@mui/material/Grid';
 
 type BooksGalleryProps = {
     books: BookWithId[];
@@ -13,14 +14,14 @@ export function BooksGallery({books,loading}: BooksGalleryProps) {
     if (loading) return <LoadingSpinner/>;
     return (
         <Box sx={{flexGrow: 1}}>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} >
 
                 {books.map((book) => (
                     <Grid
+                        size={{xs:12,sm:6,md:3}}
                         key={book.id}
-                        size={{ xs: 12,sm:6,  md: 3 }}
                     >
-                        <BookCard key={book.id} book={book}/>
+                        <BookCard book={book}/>
                     </Grid>
                 ))}
 
