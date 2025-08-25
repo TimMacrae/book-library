cb7d51b3ecf9af4ad658f61ab57699f3516e1f96
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-25 11:55:09 +0200

    update controller to return id

--- backend/src/main/java/com/ecosystem/backend/auth/AuthController.java
+++ backend/src/main/java/com/ecosystem/backend/auth/AuthController.java
@@ -1,6 +1,5 @@
 package com.ecosystem.backend.auth;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -13,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     @GetMapping
-    public ResponseEntity<String> getMe(@AuthenticationPrincipal OAuth2User user) {
+    public ResponseEntity<String> getUser(@AuthenticationPrincipal OAuth2User user) {
         System.out.println(user);
-        return ResponseEntity.ok(user.getAttributes().get("login").toString());
+        return ResponseEntity.ok(user.getAttributes().get("id").toString());
     }
 }
