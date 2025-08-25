d27644b4e51605a0590c36be5069c8236979e049
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-25 11:56:14 +0200

    add controller tests

---
+++ backend/src/test/java/com/ecosystem/backend/auth/AuthControllerTest.java
@@ -0,0 +1,29 @@
+package com.ecosystem.backend.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getUserId_shouldReturnUserId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/auth").with(oidcLogin().userInfoToken(token->
+                                token.claim("id","testId")
+                        )))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("testId"));
+    }
+
+}
