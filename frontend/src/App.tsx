50511cc7b80106ea64b5725c4af2aae7a6b37b26
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-25 11:52:57 +0200

    update App to set the user for authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,19 @@
 import './App.css'
 import {Router} from "./pages/Router"
 import {Navbar} from "./components/Navbar.tsx";
-import {routerConfig} from "./pages/routerConfig.ts";
-import axios from "axios";
-import {useEffect} from "react";
+import {useState} from "react";
+import type {User} from "./types/userType.ts";
+
 
 function App() {
-
-    function login() {
-        const host = window.location.host === routerConfig.API.HOST5173 ? routerConfig.API.HOST8080: window.location.origin
-
-        window.open(host + routerConfig.API.GITHUB_AUTH, '_self')
-    }
-
-    function loadingUser (){
-        axios.get(routerConfig.API.AUTH_USER)
-        .then((response) => {
-            console.log(response)
-        })
-    }
-
-    useEffect(() => {
-        loadingUser();
-    },[])
+    const [user, setUser] = useState<User>(undefined);
 
     return (
         <>
             <header>
-              <Navbar /> <button onClick={login}>Login</button>
+              <Navbar user={user} />
             </header>
-            <Router/>
+            <Router user={user}  setUser={setUser} />
             <footer></footer>
         </>
     )
